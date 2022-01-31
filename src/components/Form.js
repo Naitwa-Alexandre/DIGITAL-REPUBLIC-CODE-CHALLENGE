11f4084b97b947bef6c211c54f53c72402935aff
alepereira <naitwa-alexandre@gmail.com>
2022-01-31 07:29:08 -0300

    logica de toggle para portas e janelas

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Form({ idx }) {
+  const [toggle, SetToggle] = useState({
+    check1: false,
+    check2: false,
+    check3: false,
+    check4: false,
+  });
+
+  const handleToggle = (e) => {
+    SetToggle((toggle) => {
+      return {
+        ...toggle,
+        [e.target.id]: !toggle[e.target.id],
+      }
+    });
+  }
+
   return <>
     <label htmlFor={`height${idx}`}>Altura: </label>
     <input id={`height${idx}`} name='height' type="number" />
@@ -9,6 +25,26 @@ export default function Form({ idx }) {
     <input id={`width${idx}`} type="number" />
 
     <label htmlFor={`check${idx}`}>Contem portas ou janelas?</label>
-    <input id={`check${idx}`}  type='checkbox' />
+    <input onChange={ handleToggle } id={`check${idx}`}  type='checkbox' />
+
+    <div style={{ display: toggle[`check${idx}`] ? 'block' : 'none' }}>
+      <label  htmlFor={`port${idx}`}>Numero de portas: </label>
+      <select id={`port${idx}`} name="port">
+        <option value=""></option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
+
+      <label htmlFor={`window${idx}`}>Numero de janelas: </label>
+      <select id={`window${idx}`} name="window">
+        <option value=""></option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
+    </div>
   </>;
 }
